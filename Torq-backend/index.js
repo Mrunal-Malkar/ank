7c9c197de2919c393e1483e2be97d7f5f85000e1
Mrunal-Malkar <mrunalpmalkar@gmail.com>
2025-10-15 18:53:19 +0530

    torq backend (name changed)

---
+++ Torq-backend/index.js
@@ -0,0 +1,33 @@
+import { Perplexity } from "@perplexity-ai/perplexity_ai/client.js";
+import express from "express";
+import { configDotenv } from "dotenv";
+
+const app=express();
+const PORT=3000;
+configDotenv();
+
+app.get("/",(req,res)=>{
+    res.send("this is basic code");
+})
+
+app.get("/res",async(req,res)=>{
+      const client = new Perplexity({
+  apiKey:process.env(PERPLEXITY_API_KEY), // Optional if set in environment
+});
+
+// Non-streaming request
+const response = await client.chat.completions.create({
+  model: 'sonar-pro',
+  messages: [
+    { role: 'user', content: 'When is board exam of ssc board for 12th standard (india) 2026?' }
+  ],
+});
+
+console.log(response.choices[0].message.content);
+const r=response.choices[0].message.content;
+res.send(r);
+})
+
+app.listen(PORT,(req,res)=>{
+    console.log(`your app is running on port:${PORT}`);
+})
--- ank.ai/src/pages/live.tsx
+++
@@ -1,8 +0,0 @@
-
-const Live = () => {
-  return (
-  <h1>sad</h1>
-  )
-}
-
-export default Live
