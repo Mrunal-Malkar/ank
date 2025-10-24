039c485a8a52cc203a4f5f21fde237954677aab5
Mrunal-Malkar <mrunalpmalkar@gmail.com>
2025-10-24 15:59:59 +0530

    added dark as bg default

--- Torq.ai/src/App.tsx
+++ Torq.ai/src/App.tsx
@@ -6,7 +6,7 @@ import { NavbarDemo } from './components/navbar'
 function App() {
 
   return (
-    <div className='w-screen flex flex-col justify-center items-start'>
+    <div className='w-screen flex flex-col justify-center items-start bg-black text-white'>
       <NavbarDemo/>
       <Outlet />
     </div>
