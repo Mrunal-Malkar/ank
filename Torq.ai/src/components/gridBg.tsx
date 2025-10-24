05838ab8e27e1c57056db2abf5a417ba53e406da
Mrunal-Malkar <mrunalpmalkar@gmail.com>
2025-10-24 11:53:30 +0530

    increased font size of torq

--- Torq.ai/src/components/gridBg.tsx
+++ Torq.ai/src/components/gridBg.tsx
@@ -16,7 +16,7 @@ export function GridBackground() {
       {/* Radial gradient for the container to give a faded look */}
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
       <div className="flex flex-col justify-center items-center">
-      <p className="relative z-2 flex flex-col gap-y-2 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-9xl">
+      <p className="relative z-2 flex flex-col gap-y-2 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-8xl font-bold text-transparent sm:text-9xl">
         Torq
       </p>
       <h6 className="sm:text-3xl text-xl text-center text-neutral-400">
